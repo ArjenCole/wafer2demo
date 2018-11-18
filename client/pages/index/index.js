5f719cf112ec9710e518574d17609d7f4c79145c
ArjenCole-Desktop <jinhesky@hotmail.com>
2018-11-18 16:32:28 +0800

    打开小程序后自动登陆微信账号
    1.登陆函数移入onShow
    2.wxml中按钮名称改为“未登录”

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -11,38 +11,50 @@ Page({
         requestResult: '',
         imgUrl:''
     },
+    
+    onShow: function () {
+      this.autoGetUserInfo()
+    },
 
     bindGetUserInfo: function () {
-        if (this.data.logged) return
-
-        util.showBusy('正在登录')
-
-        const session = qcloud.Session.get()
-
-        if (session) {
-            qcloud.loginWithCode({
-                success: res => {
-                    this.setData({ userInfo: res, logged: true })
-                    util.showSuccess('登录成功')
-                },
-                fail: err => {
-                    console.error(err)
-                    util.showModel('登录错误', err.message)
-                }
-            })
-        } else {
-            qcloud.login({
-                success: res => {
-                    this.setData({ userInfo: res, logged: true })
-                    util.showSuccess('登录成功')
-                },
-                fail: err => {
-                    console.error(err)
-                    util.showModel('登录错误', err.message)
-                }
-            })
-        }
+      //this.autoGetUserInfo()
     },
+
+    // 获得用户信息
+    autoGetUserInfo:function(){
+      if (this.data.logged) return
+
+      util.showBusy('正在登录')
+
+      const session = qcloud.Session.get()
+
+      if (session) {
+        qcloud.loginWithCode({
+          success: res => {
+            this.setData({ userInfo: res, logged: true })
+            util.showSuccess('登录成功')
+          },
+          fail: err => {
+            console.error(err)
+            util.showModel('登录错误', err.message)
+          }
+        })
+      } else {
+        qcloud.login({
+          success: res => {
+            this.setData({ userInfo: res, logged: true })
+            util.showSuccess('登录成功')
+          },
+          fail: err => {
+            console.error(err)
+            util.showModel('登录错误', err.message)
+          }
+        })
+      }
+
+
+    },
+
 
     // 切换是否带有登录态
     switchRequestMode: function (e) {
